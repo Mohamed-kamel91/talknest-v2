3d280c511bc6067d5238f570dea014c469490d25
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-04 16:53:16 +0300

    refactor(core): reorganize exports in index.ts for better structure and clarity

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,26 @@
-export { NumberUtil } from './utils/number-utils';
-export { testSupportRoot } from './utils/paths';
+// Application
+export { Collection } from './application/collection';
+export { type IHandle } from './application/eventHandler';
+export { ReadModel } from './application/readModel';
+export {
+  type UseCase,
+  type UseCaseResponse,
+  Result,
+  fail,
+  success,
+} from './application/useCase';
+export { type Request } from './application/request';
+
+// Domain
+export { AggregateRoot } from './domain/aggregateRoot';
+export {
+  DomainEvent,
+  type DomainEventStatus,
+} from './domain/domainEvent';
+export { ValueObject } from './domain/valueObject';
+export { type EventModel } from './domain/eventModel';
+
+// Utils
+export { NumberUtil } from './utils/number-util';
+export { DateUtil } from './utils/date-util';
+export { TextUtil } from './utils/text-util';
