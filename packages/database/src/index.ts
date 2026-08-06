0b53cdc86b50e2a07de5c6e1c0fa5edfbc036ed2
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-06 20:10:01 +0300

    refactor(database): export models and PrismaClient from generated client

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,9 @@
 export { Database } from './database';
+export {
+  Comment as CommentModel,
+  Member as MemberModel,
+  Post as PostModel,
+  User as UserModel,
+  Vote as VoteModel,
+  PrismaClient,
+} from './prisma/generated/client';
