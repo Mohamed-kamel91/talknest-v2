6d085f323e6a610ef837291df6b3efe6ffa0e412
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-07 19:21:19 +0300

    feat(bus): implement InMemoryEventBus with event publishing and subscription features

---
+++ packages/bus/src/ports/eventBus.ts
@@ -0,0 +1,15 @@
+import { DomainEvent } from '@talknest/core';
+
+export interface EventBus {
+  initialize(): Promise<any>;
+  stop(): Promise<any>;
+  publishEvents(events: DomainEvent[]): void;
+  subscribe<T extends DomainEvent>(
+    eventTypeName: string,
+    handler: (event: T) => void,
+  ): void;
+  unsubscribe(
+    eventTypeName: string,
+    handler: (event: DomainEvent) => void,
+  ): void;
+}
