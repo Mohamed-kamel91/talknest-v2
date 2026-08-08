3464bc2c659470315d39e895c4630c1ad6685e7e
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-08 20:13:23 +0300

    refactor: update exports in database index to use PrismaDatabase and type annotations

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,9 +1,10 @@
-export { Database } from './database';
+export { PrismaDatabase, type IDatabase } from './database';
 export {
-  Comment as CommentModel,
-  Member as MemberModel,
-  Post as PostModel,
-  User as UserModel,
-  Vote as VoteModel,
+  type Member as MemberModel,
+  type Post as PostModel,
+  type PostVote as PostVoteModel,
+  type Comment as CommentModel,
+  type CommentVote as CommentVoteModel,
+  type Event as EventModel,
   PrismaClient,
 } from './prisma/generated/client';
