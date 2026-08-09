ce6fd184c6f35c25a4021e9475539b0afcbefc52
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-09 18:46:08 +0300

    refactor: restructure database class to implement IDatabase interface

--- packages/database/src/database.ts
+++ packages/database/src/database.ts
@@ -1,7 +1,15 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './prisma/generated/client';
+import './loadEnv';
 
-export class Database {
+
+export interface IDatabase {
+  getConnection(): PrismaClient;
+  connect(): Promise<void>;
+  disconnect(): Promise<void>;
+}
+
+export class PrismaDatabase implements IDatabase {
   private client: PrismaClient;
 
   constructor() {
@@ -13,14 +21,21 @@ export class Database {
   }
 
   private createClient() {
-    const connectionString = `${process.env.DATABASE_URL}`;
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not set.');
+    }
+
     const adapter = new PrismaPg({ connectionString });
     const client = new PrismaClient({ adapter });
     return client;
   }
 
   public async connect() {
+    console.log('Starting the database connection...');
     await this.client.$connect();
+    console.log('Connected to the prisma database');
   }
 
   public async disconnect() {
