d903cb8b50af611b7253e2b222b5ed8368bed1d0
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-15 18:55:29 +0300

    Refactor(api): update API client to include comments and members APIs, and adjust response types

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,35 +1,47 @@
 import { createMarketingAPI } from './marketing';
 import { createPostsAPI } from './posts';
 import { createUsersAPI } from './users';
+import { createCommentsAPI } from './comments';
+import { createMembersAPI } from './members';
+import { createVotesAPI } from './votes';
 
 export type APIError<U> = {
   type: U;
-  code: number;
   message: string;
 };
 
 export type SuccessResponse<Data> = {
   success: true;
   data: Data;
+  statusCode: number;
   error: null;
 };
 
 export type FailureResponse<ErrorType> = {
   success: false;
   data: null;
+  statusCode: number;
   error: APIError<ErrorType>;
 };
 
 export type APIResponse<T, U> =
   SuccessResponse<T> | FailureResponse<U>;
 
+export const getAuthHeaders = (token?: string) => ({
+  headers: token
+    ? {
+        Authorization: `Bearer ${token}`,
+      }
+    : {},
+});
+
 export const createAPIClient = (apiURL: string) => {
   return {
+    comments: createCommentsAPI(apiURL),
     marketing: createMarketingAPI(apiURL),
+    members: createMembersAPI(apiURL),
     posts: createPostsAPI(apiURL),
     users: createUsersAPI(apiURL),
-    // members: createMembersAPI(apiURL),
-    // comments: createCommentssAPI(apiURL),
-    // votes: createVotesAPI(apiURL),
+    votes: createVotesAPI(apiURL),
   };
 };
