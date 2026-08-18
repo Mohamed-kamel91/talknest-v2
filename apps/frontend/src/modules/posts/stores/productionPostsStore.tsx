cb9b93405c18df5cafa32d22ae862aab9e9721e0
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-18 22:15:53 +0300

    feat(frontend/linting): update code structure for improved readability and maintainability

---
+++ apps/frontend/src/modules/posts/stores/productionPostsStore.tsx
@@ -0,0 +1,52 @@
+import { makeAutoObservable } from 'mobx';
+
+import { APIClient } from '@talknest/api';
+import { CreatePostInput, GetPostsQuery } from '@talknest/api/posts';
+
+import { PostDm } from '../domain/postDm';
+import { IPostsStore } from './postsStore';
+
+import { AuthStore } from '@/modules/auth/stores/authStore';
+
+export class PostsStore implements IPostsStore {
+  public postsDm: PostDm[];
+
+  constructor(
+    private api: APIClient,
+    private authStore: AuthStore,
+  ) {
+    makeAutoObservable(this);
+    this.postsDm = [];
+  }
+
+  async getPostBySlug(slug: string): Promise<PostDm | null> {
+    const response = await this.api.posts.getPostBySlug(slug);
+    if (response.success && response.data) {
+      return PostDm.fromDTO(response.data);
+    }
+    return null;
+  }
+
+  async getPosts(query?: GetPostsQuery): Promise<PostDm[]> {
+    const getPostsResponse = await this.api.posts.getPosts({
+      sort: query?.sort ?? 'popular',
+    });
+    const postDTOs = getPostsResponse.data;
+    if (!postDTOs) {
+      return [];
+    }
+    this.postsDm = postDTOs.map((postDTO) => PostDm.fromDTO(postDTO));
+    return this.postsDm;
+  }
+
+  async create(input: CreatePostInput): Promise<PostDm> {
+    const authToken = this.authStore.getToken() ?? '';
+    const response = await this.api.posts.create(input, authToken);
+    if (!response.data) {
+      throw new Error('Failed to create post');
+    }
+    const newPost = PostDm.fromDTO(response.data);
+    this.postsDm.push(newPost);
+    return newPost;
+  }
+}
--- apps/frontend/tests/shared/components/notifications-component.ts
+++
@@ -1,9 +0,0 @@
-import { PuppeteerPageDriver } from '../driver';
-
-export class NotificationsComponent {
-  constructor(private driver: PuppeteerPageDriver) {}
-
-  public async isSuccessToastVisible() {}
-
-  public async isFailureToastVisible() {}
-}
