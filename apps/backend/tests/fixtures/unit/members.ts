096b69e175e1cbb9b50c00010effd324c87bb688
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-08-18 21:38:57 +0300

    feat(tests): add end-to-end and unit tests for posts and members functionality

---
+++ apps/backend/tests/fixtures/unit/members.ts
@@ -0,0 +1,46 @@
+// Fixtures (hvut fixtures)
+import { reputationLevel } from '@talknest/api/members';
+
+import { Member } from '../../../src/modules/members/domain/member';
+import { MemberUsername } from '../../../src/modules/members/domain/member-username';
+import { CreatePost } from '../../../src/modules/posts/application/use-cases/create-post/create-post';
+import { PostComment } from '../../../src/modules/comments/application/use-cases/post-comment/post-comment';
+
+export function setupTestWithLevel2Member(
+  useCase: CreatePost | PostComment,
+) {
+  jest.resetAllMocks();
+
+  const level2Member = Member.toDomain({
+    userId: '8be25ac7-49ff-43be-9f22-3811e268e0bd',
+    username: MemberUsername.toDomain('jill-12345'),
+    reputationScore: 10,
+    reputationLevel: reputationLevel.Level2,
+    id: 'bf6b4773-feea-44cd-a951-f0ffd68625ea',
+  });
+
+  useCase['memberRepository'].getMemberById = jest
+    .fn()
+    .mockResolvedValue(level2Member);
+
+  return level2Member;
+}
+
+export function setupTestWithLevel1Member(
+  useCase: CreatePost | PostComment,
+) {
+  const level1MemberOrError = Member.create({
+    userId: '8be25ac7-49ff-43be-9f22-3811e268e0bd',
+    username: 'jill1234',
+  });
+
+  expect(level1MemberOrError.isSuccess()).toBe(true);
+
+  const member = level1MemberOrError.getValue();
+
+  useCase['memberRepository'].getMemberById = jest
+    .fn()
+    .mockResolvedValue(member);
+
+  return member;
+}
